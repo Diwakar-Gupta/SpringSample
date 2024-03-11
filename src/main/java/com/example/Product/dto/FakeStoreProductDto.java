52d2b614e8cd644c2b478a09fc42c543347b0114
Diwakar Gupta <39624018+Diwakar-Gupta@users.noreply.github.com>
2024-03-11 18:49:44 +0530

    resttemplate

---
+++ src/main/java/com/example/Product/dto/FakeStoreProductDto.java
@@ -0,0 +1,8 @@
+package com.example.Product.dto;
+
+public class FakeStoreProductDto {
+    Long id;
+    String title;
+    double price;
+    String description;
+}
--- src/main/java/com/example/Product/service/ProductService.java
+++ src/main/java/com/example/Product/service/ProductService.java
@@ -1,15 +1,23 @@
 package com.example.Product.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.Product.dto.FakeStoreProductDto;
 
 @Service
 public class ProductService {
+
+    RestTemplate restTemplate = new RestTemplate();
+    String url = "https://fakestoreapi.com/";
 
     public String getAllProducts() {
         return "All Products";
     }
 
     public String getProductbyId(Long id) {
+        var response = restTemplate.getForObject(url + "/products/" + id, FakeStoreProductDto.class);
+
         return "Product from Service with id: " + id;
     }
 }
